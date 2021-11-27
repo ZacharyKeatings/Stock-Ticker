2693a115d5749fc8909eb01c8474d4236f88d569
Zachary Keatings <zachary.keatings@gmail.com>
2021-11-26 23:12:30 -0500

    Made create_stocks to populate list before game Need to now rework increase_value(), decrease_value()

--- StockTickerRewrite.py
+++ StockTickerRewrite.py
@@ -3,6 +3,7 @@ import random
 class Player:
     "Player stats"
 
+    players = []
     #stocks refer to quantity of given stock. money is available money. name is chosen by user
     def __init__(self, name = "", money = 5000, gold = 0, silver = 0, oil = 0, bonds = 0, grain = 0, industrial = 0):
         self.name = name
@@ -14,6 +15,9 @@ class Player:
         self.grain = grain
         self.industrial = industrial
 
+        #Subclass Humans
+        #Subclass Bots
+
     #Creates user chosen number of bots (1 - 8)
     def create_bots():
         num_bots = ask_question("How many bots would you like to play? Choose between 1 - 8.\n", range(1, 8))
@@ -24,10 +28,15 @@ class Player:
         num_humans = ask_question("How many people are playing? Choose between 2 - 8.\n", range(2, 8))
         return num_humans
 
-    #Allows user to customize name of current player
-    def name_players(player):
-        name = ask_question(f"What is {player}'s name?\n")
-        return name
+    #Creates names for all bot + human players
+    def name_player(total_players):
+        total_players = 5
+        for num in range(1, total_players+1):
+            # player = Player(name=f"Player {num}")
+            player = Player(name=ask_question(f"What is Player {num}'s name?\n"))
+            print(f"{player.name} has {player.money} bucks.")
+            print(f"Player {num} is now named: {player.name}")
+            Player.players.append(player)
 
     #provide the (stock name, price of stock, amount of money player has) 
     def buy_stock(stock, cost, player_money):
@@ -41,12 +50,21 @@ class Stock:
     "Stock value"
 
     #Change stock names here:
-    stock_list = ["Gold", "Silver", "Oil", "Bonds", "Grain", "Industrial"]
+    stock_name = ["Gold", "Silver", "Oil", "Bonds", "Grain", "Industrial"]
+    stocks = []
 
     #Set stock starting cost here:
     def __init__(self, name, value = 195):
         self.name = name
         self.value = value
+
+    #Create list of stocks with data based on Stock class
+    def create_stocks():
+        for num in range(0, 5):
+            print(f"Now working on {Stock.stock_name[num]}")
+            stock = Stock(name=Stock.stock_name[num])
+            Stock.stocks.append(stock)
+            print(Stock.stocks[num].name)
 
     #Called from roll(), handles adding to stock value
     def increase_value(stock, amount):
@@ -57,31 +75,31 @@ class Stock:
             print("Now in double_stock()")
 
         print("now in increase_value()")
-        if stock == Stock.stock_list[0]:
+        if stock == Stock.stock_name[0]:
             print("Now in increase_value(Gold)")
             stock1.value = stock1.value + amount
             print(stock1.value)
             if stock1.value > 195:
                 double_stock(stock)
-        elif stock == Stock.stock_list[1]:
+        elif stock == Stock.stock_name[1]:
             print("Now in increase_value(Silver)")
             stock2.value = stock2.value + amount
             print(stock2.value)
             if stock2.value > 195:
                 double_stock(stock)
-        elif stock == Stock.stock_list[2]:
+        elif stock == Stock.stock_name[2]:
             print("Now in increase_value(Oil)")
             stock3.value = stock3.value + amount
             print(stock3.value)
             if stock3.value > 195:
                 double_stock(stock)
-        elif stock == Stock.stock_list[3]:
+        elif stock == Stock.stock_name[3]:
             print("Now in increase_value(Bonds)")
             stock4.value = stock4.value + amount
             print(stock4.value)
             if stock4.value > 195:
                 double_stock(stock)
-        elif stock == Stock.stock_list[4]:
+        elif stock == Stock.stock_name[4]:
             print("Now in increase_value(Grain)")
             stock5.value = stock5.value + amount
             print(stock5.value)
@@ -103,31 +121,31 @@ class Stock:
             print("Now in split_stock()")
 
         print("Now in decrease_value()")
-        if stock == Stock.stock_list[0]:
+        if stock == Stock.stock_name[0]:
             print("Now in decrease_value(Gold)")
             stock1.value = stock1.value - amount
             print(stock1.value)
             if stock1.value < 5:
                 split_stock(stock)
-        elif stock == Stock.stock_list[1]:
+        elif stock == Stock.stock_name[1]:
             print("Now in decrease_value(Silver)")
             stock2.value = stock2.value - amount
             print(stock2.value)
             if stock2.value < 5:
                 split_stock(stock)
-        elif stock == Stock.stock_list[2]:
+        elif stock == Stock.stock_name[2]:
             print("Now in decrease_value(Oil)")
             stock3.value = stock3.value - amount
             print(stock3.value)
             if stock3.value < 5:
                 split_stock(stock)
-        elif stock == Stock.stock_list[3]:
+        elif stock == Stock.stock_name[3]:
             print("Now in decrease_value(Bonds)")
             stock4.value = stock4.value - amount
             print(stock4.value)
             if stock4.value < 5:
                 split_stock(stock)
-        elif stock == Stock.stock_list[4]:
+        elif stock == Stock.stock_name[4]:
             print("Now in decrease_value(Grain)")
             stock5.value = stock5.value - amount
             print(stock5.value)
@@ -147,7 +165,7 @@ class Stock:
 class Dice:
     "Dice properties"
 
-    stock_name = Stock.stock_list
+    stock_name = Stock.stock_name
     action = ["Up", "Down", "Dividend"]
     amount = [5, 10, 20]
 
@@ -167,7 +185,11 @@ class Dice:
             Stock.dividend(stock, amount)
 
 class Menu:
-    
+    "All menu screens"
+    stocks = Stock.stock_name #Answer for stock name
+    action = ["Buy", "Sell", "Done", ""]
+    amount = [range(1-1000)]
+
     def main_screen():
         pass
 
@@ -185,90 +207,19 @@ def ask_question(question, answers=None):
 #This is a temporary function for debugging
 ##########
 #Create the stocks:
-stock1 = Stock(Stock.stock_list[0])
-stock2 = Stock(Stock.stock_list[1])
-stock3 = Stock(Stock.stock_list[2])
-stock4 = Stock(Stock.stock_list[3])
-stock5 = Stock(Stock.stock_list[4])
-stock6 = Stock(Stock.stock_list[5])
+stock1 = Stock(Stock.stock_name[0])
+stock2 = Stock(Stock.stock_name[1])
+stock3 = Stock(Stock.stock_name[2])
+stock4 = Stock(Stock.stock_name[3])
+stock5 = Stock(Stock.stock_name[4])
+stock6 = Stock(Stock.stock_name[5])
 ##########
 
-Dice.roll()
+Stock.create_stocks()
 
-##########
-#Create the player placeholder:
-player1 = Player()
-player2 = Player()
-player3 = Player()
-player4 = Player()
-player5 = Player()
-player6 = Player()
-player7 = Player()
-player8 = Player()
-##########
+# Dice.roll()
 
-human_players = 0
-bot_players = 0
-total_players = human_players + bot_players
-current_player = 1
-
-#Creating number of players as chosen by user based on Player class, as well as choosing names.
-while current_player <= total_players:
-    if current_player == 1:
-        #Player 1
-        player1 = Player()
-        player1.name = Player.name_players("Player 1")
-        print(f"{player1.name} has {player1.money} bucks.")
-        current_player += 1
-        print(f"{current_player}")
-    elif current_player == 2:
-        #player 2
-        player2 = Player()
-        player2.name = Player.name_players("Player 2")
-        print(f"{player2.name} has {player2.money} bucks.")
-        current_player += 1
-        print(f"{current_player}")
-    elif current_player == 3:
-        #Player 3
-        player3 = Player()
-        player3.name = Player.name_players("Player 3")
-        print(f"{player3.name} has {player3.money} bucks.")
-        current_player += 1
-        print(f"{current_player}")
-    elif current_player == 4:
-        #Player 4
-        player4 = Player()
-        player4.name = Player.name_players("Player 4")
-        print(f"{player4.name} has {player4.money} bucks.")
-        current_player += 1
-        print(f"{current_player}")
-    elif current_player == 5:
-        #Player 5
-        player5 = Player()
-        player5.name = Player.name_players("Player 5")
-        print(f"{player5.name} has {player5.money} bucks.")
-        current_player += 1
-        print(f"{current_player}")
-    elif current_player == 6:
-        #Player 6
-        player6 = Player()
-        player6.name = Player.name_players("Player 6")
-        print(f"{player6.name} has {player6.money} bucks.")
-        current_player += 1
-        print(f"{current_player}")
-    elif current_player == 7:
-        #Player 7
-        player7 = Player()
-        player7.name = Player.name_players("Player 7")
-        print(f"{player7.name} has {player7.money} bucks.")
-        current_player += 1
-        print(f"{current_player}")
-    elif current_player == 8:
-        #Player 8
-        player8 = Player()
-        player8.name = Player.name_players("Player 8")
-        print(f"{player8.name} has {player8.money} bucks.")
-        print(f"{current_player}")
-        current_player += 1
-    else:
-        break
+# human_players = 0
+# bot_players = 0
+# total_players = human_players + bot_players
+# current_player = 1
